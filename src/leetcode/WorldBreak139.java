8b1c63d9980f7fae0bf548ec8a97bf9059317e0b
zhiyu <gaozhiyu301@gmail.com>
2020-10-17 18:15:41 +0800

    leet code question 139
     some improvement

--- src/leetcode/WorldBreak139.java
+++ src/leetcode/WorldBreak139.java
@@ -1,11 +1,14 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class WorldBreak139 {
 	public boolean globalResult = false;
     public boolean wordBreak(String s, List<String> wordDict) {
+    	if(singleElementExist(s, wordDict))
+    		return true;
     	recursiveCall(s, wordDict);
         return this.globalResult;
     }
@@ -21,6 +24,19 @@ public class WorldBreak139 {
     	}
     }
     
+    public boolean singleElementExist(String s, List<String> wordDict){
+    	List<String> singleElement = new ArrayList<>();
+    	boolean result = true;
+    	for (int i=0; i<s.length();i++) {
+    		String str = String.valueOf(s.charAt(i));
+    		if(!wordDict.contains(str)) {
+    			result =false;
+    			break;
+    		}
+    	}
+    	return result;
+    }
+    
     
     public static void main(String[] args) {
     	String str = "HelloWord";
@@ -31,7 +47,8 @@ public class WorldBreak139 {
     			//[]
     	String[] wordDict = {"abbcbda","cbdaaa","b","dadaaad","dccbbbc","dccadd","ccbdbc","bbca","bacbcdd","a","bacb","cbc","adc","c","cbdbcad","cdbab","db","abbcdbd","bcb","bbdab","aa","bcadb","bacbcb","ca","dbdabdb","ccd","acbb","bdc","acbccd","d","cccdcda","dcbd","cbccacd","ac","cca","aaddc","dccac","ccdc","bbbbcda","ba","adbcadb","dca","abd","bdbb","ddadbad","badb","ab","aaaaa","acba","abbb"};
     	List<String> strList = Arrays.asList(wordDict); 
-    	boolean result = new WorldBreak139().wordBreak(s, strList) ;
+    	//boolean result = new WorldBreak139().wordBreak(s, strList) ;
+    	boolean result = new WorldBreak139().singleElementExist(s, strList) ;
     	System.out.println(result);
     }
 }
