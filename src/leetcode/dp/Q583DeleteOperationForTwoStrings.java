714e3962dfc785c6024e4d4fcac2aa46e726dcd7
zhiyu <gaozhiyu301@gmail.com>
2020-11-13 12:32:33 +0800

    work on the Q583

--- src/leetcode/dp/Q583DeleteOperationForTwoStrings.java
+++ src/leetcode/dp/Q583DeleteOperationForTwoStrings.java
@@ -2,30 +2,48 @@ package leetcode.dp;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
 
 public class Q583DeleteOperationForTwoStrings {
 	
-	private ConcurrentHashMap<String, List<Integer>> hashMap= new ConcurrentHashMap<String, List<Integer>>();
+	
 
-    public int minDistance(String word1, String word2) {
-    	if(word1.length()> word2.length()) {
-    		String temp = word2;
-    		word2 = word1;
-    		word1 = temp;
-    	}
+public int minDistance(String word1, String word2) {
+		
+		HashMap<String, List<Integer>> hashMap= new HashMap<String, List<Integer>>();
+
     	int comonlength = 0;
-    	StringBuilder sb = new StringBuilder();
+    	StringBuffer sb = new StringBuffer();
     	for(int i = 0; i< word1.length();i++ ) {
     		String substr = String.valueOf(word1.charAt(i));
     		if(word2.contains(substr)) {
     			sb.append(substr);
     		}
     	}
-    	String word1AfterFiter=sb.toString();
+    	String word1AfterFiter="";
+        String word2AfterFiter="";
+    	StringBuffer sb1 = new StringBuffer();
+    	for(int i = 0; i< word2.length();i++ ) {
+    		String substr = String.valueOf(word2.charAt(i));
+    		if(word1.contains(substr)) {
+    			sb1.append(substr);
+    		}
+    	}
+        if(sb.length() < sb1.length()){
+            word1AfterFiter = sb.toString();
+            word2AfterFiter = sb1.toString();
+        }else{
+            word2AfterFiter = sb.toString();
+            word1AfterFiter = sb1.toString();
+        }
+        System.out.println(word1AfterFiter.length() + " " + word1AfterFiter);
+        System.out.println(word2AfterFiter.length() + " " + word2AfterFiter);
     	List<Integer> lastIndex = new ArrayList();
     	List<Integer> maxCommonLength = new ArrayList();
     	List<String> subStringSet = new ArrayList(); 
+        List<Integer> lastIndexPro = new ArrayList();
+    	List<Integer> maxCommonLengthPro = new ArrayList();
+    	List<String> subStringSetPro = new ArrayList();
     	if(word1AfterFiter.length()==0) {
     		return word1.length()+ word2.length();
     	}else {
@@ -34,16 +52,16 @@ public class Q583DeleteOperationForTwoStrings {
     		maxCommonLength.add(0);
     		subStringSet.add("");
     		
-    		lastIndex.add(word2.indexOf(firstChar));
+    		lastIndex.add(word2AfterFiter.indexOf(firstChar));
     		maxCommonLength.add(1);
     		subStringSet.add(firstChar);
     		for(int i =1; i < word1AfterFiter.length();i++ ) {
     			String nextChar = String.valueOf(word1AfterFiter.charAt(i));
-    	    	List<Integer> lastIndexPro = new ArrayList();
-    	    	List<Integer> maxCommonLengthPro = new ArrayList();
-    	    	List<String> subStringSetPro = new ArrayList();
+    	    	lastIndexPro.clear();
+    	    	maxCommonLengthPro.clear();
+    	    	subStringSetPro.clear();
 
-    	    	List<Integer> indiceList = getIndices(word2,nextChar);
+    	    	List<Integer> indiceList = getIndices(word2AfterFiter,nextChar,hashMap);
     	    	for(int j = 0; j< subStringSet.size();j++) {
     	    		int lastestIndex =  lastIndex.get(j);
     	    		int latestCommonLength = maxCommonLength.get(j);
@@ -54,7 +72,7 @@ public class Q583DeleteOperationForTwoStrings {
     	    				latestCommonLength++;
     	    				maxCommonLengthPro.add(latestCommonLength);
     	    				lastIndexPro.add(index);
-    	    				subStringSetPro.add(curSubStr + curSubStr);
+    	    				subStringSetPro.add(curSubStr + nextChar);
     	    				break;
     	    			}
     	    		}
@@ -80,7 +98,7 @@ public class Q583DeleteOperationForTwoStrings {
     }
 	
     
-    public List<Integer> getIndices(String word, String charecter){
+    public List<Integer> getIndices(String word, String charecter,HashMap<String, List<Integer>> hashMap){
     	if(hashMap.get(charecter)!=null)
     		return hashMap.get(charecter);
     	List<Integer> indices = new ArrayList();
@@ -109,7 +127,11 @@ public class Q583DeleteOperationForTwoStrings {
 		 * obj.getIndices("hello","l"); for(Integer i : intList2) {
 		 * System.out.println(i); }
 		 */
-		int minDistance = new Q583DeleteOperationForTwoStrings().minDistance("pneumonoultramicroscopicsilicovolcanoconiosis","ultramicroscopical");
+    	String str1= "szwokpjlgqgogbgpjaczcmtjhzgldwinqfxbcxgghitcinmtdwnnpsmnmhfrhrgwncvcizaze";
+    	String str2= "spjlqggpzcgdxxtdwnrvca";
+    	
+		//int minDistance = new Q583DeleteOperationForTwoStrings().minDistance("pneumonoultramicroscopicsilicovolcanoconiosis","ultramicroscopical");
+    	int minDistance = new Q583DeleteOperationForTwoStrings().minDistance(str1,str2);
 		System.out.println("minDistance : " + minDistance);
 	}
     
